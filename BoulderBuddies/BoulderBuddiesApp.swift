3e833bc5617fbe3788f92400ae1806f1b5d175fc
James Yeap <james.yeap@accenture.com>
2022-07-17 19:16:05 +0800

    Initial Commit

---
+++ BoulderBuddies/BoulderBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderBuddiesApp.swift
+//  BoulderBuddies
+//
+//  Created by james.yeap on 17/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BoulderBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoulderBuddies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoulderBuddies
+//
+//  Created by james.yeap on 17/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
